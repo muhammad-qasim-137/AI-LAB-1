5279355f682d16b8b745560324a4774dc305fb55
qasim <k250137@nu.edu.pk>
2026-08-26 23:52:42 +0500

    Delete q5.py

--- q5.py
+++
@@ -1,8 +0,0 @@
-value = float(input("enter the number: "))
-c = int(input("enter number of members in list: "))
-nums = [0]*c
-for i in range(c):
-    nums[i] = int(input("enter number: "))
-    if(nums[i]<value):
-        nums[i] = 0
-print(nums)
